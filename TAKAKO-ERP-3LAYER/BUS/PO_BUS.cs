9ab423bdac0067dd34a40eb8875ac83ff7d0ec3a
Quoc Hung <39042165+hungdq1991@users.noreply.github.com>
2020-04-17 14:23:12 +0700

    2020/04/17: Hưng xóa source thừa

--- TAKAKO-ERP-3LAYER/BUS/PO_BUS.cs
+++ TAKAKO-ERP-3LAYER/BUS/PO_BUS.cs
@@ -10,21 +10,6 @@ namespace TAKAKO_ERP_3LAYER.BUS
 {
     public class PO_BUS
     {
-        /// <constructor>
-        /// Constructor PO_BUS
-        /// </constructor>
-        public PO_BUS()
-        {
-            //_userDAO = new USER_DAO();
-        }
 
-        /// <method>
-        /// Insert PO to database
-        /// </method>
-        public Boolean insertPO(string _receiveNo)
-        {
-
-            return false;
-        }
     }
 }
